bb76a0bde1fa3f3114f60b48448b84245313e04a
Lucas Grams <seu_email_pessoal@exemplo.com>
2026-06-17 21:12:20 -0300

    TJ-1 teste commit

--- src/main/java/com/casamento/TuaniJoao/TuaniJoaoApplication.java
+++ src/main/java/com/casamento/TuaniJoao/TuaniJoaoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TuaniJoaoApplication {
-
+//teste commit
 	public static void main(String[] args) {
 		SpringApplication.run(TuaniJoaoApplication.class, args);
 	}
