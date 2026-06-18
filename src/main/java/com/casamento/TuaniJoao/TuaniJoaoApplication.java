316e5a413d09928c8b0e98e0519b7d997b0ff878
Lucas Grams <seu_email_pessoal@exemplo.com>
2026-06-17 21:14:20 -0300

    TJ-1 revert test commit

--- src/main/java/com/casamento/TuaniJoao/TuaniJoaoApplication.java
+++ src/main/java/com/casamento/TuaniJoao/TuaniJoaoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TuaniJoaoApplication {
-//teste commit
 	public static void main(String[] args) {
 		SpringApplication.run(TuaniJoaoApplication.class, args);
 	}
