57cb779511feb5e4a34fbb3027c9ac96da148668
Lucas Grams <seu_email_pessoal@exemplo.com>
2026-06-24 22:54:12 -0300

    TJ-2 correcao de telas

--- src/main/java/com/casamento/TuaniJoao/Config/GlobalExceptionHandler.java
+++ src/main/java/com/casamento/TuaniJoao/Config/GlobalExceptionHandler.java
@@ -8,10 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionHandler {
 
     // 1. Trata casos de "Não Encontrado" (Erro 404)
