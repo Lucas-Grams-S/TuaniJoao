9717acb67f4b42d2920f3557884693414ee9c8f6
Lucas Grams <seu_email_pessoal@exemplo.com>
2026-08-04 10:15:14 -0300

    TJ-7 fix route

--- src/main/java/com/casamento/TuaniJoao/Config/WebSecurityConfig.java
+++ src/main/java/com/casamento/TuaniJoao/Config/WebSecurityConfig.java
@@ -9,10 +9,11 @@ public class WebSecurityConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        //TODO: configurar o dominio quando tiver
-        registry.addMapping("/api/**")
-                // Se no futuro vocês registrarem um domínio (ex: www.tuaniejoao.com.br), troque o asterisco por ele
-                .allowedOrigins("*")
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://casamento-tuani-joao.com.br",
+                        "http://www.casamento-tuani-joao.com.br"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(3600);
